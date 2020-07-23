2a2114b7dce4c67827db34f256d2cd71384f23f0
edoardo <iweek16@gmail.com>
2020-07-23 12:03:21 +0200

    Funzionante

--- slider.js
+++ slider.js
@@ -1,20 +1,31 @@
 class Carousel{
 
+    get nImg(){return this._nImg}
+    get lastIndex(){ return this._nImg-1}
+
     constructor(el){
 
-        this.el = document.createElement('div')
-        this.el.classList.add('carousel')
+        // this.el = document.createElement('div')
+        // this.el.classList.add('wrapper')
+        this.el         = el
+        this.header     = el.querySelector('header')
+        this.wrapper    = el.querySelector('main')
+        this.footer     = el.querySelector('footer')
 
         this.btnPrev = document.createElement('button')
-        this.btnPrev.value = `<`
+        this.btnPrev.textContent = `<`
         this.btnNext = document.createElement('button')
-        this.btnNext.value = `>`
+        this.btnNext.textContent = `>`
 
-        this.el.appendChild(this.btnNext)
-        this.el.appendChild(this.btnPrev)
+        this.footer.appendChild(this.btnPrev)
+        this.footer.appendChild(this.btnNext)
 
-        el.appendChild(this)
+        // el.appendChild(this.el)
         
+        this._index     = 0
+        this._offset    = 100 
+        this._nImg      = this.el.querySelectorAll('img').length
+
         this.addEventListener()
     }
 
@@ -23,14 +34,28 @@ class Carousel{
         this.btnPrev.addEventListener('click', this.goPrev.bind(this))
     }
 
-    goTo(index){//TODO }
-
-    goNext(){
-
+    goTo(index){//TODO
     }
 
-
+    goNext(){
+        if(this._index == this._nImg-1){
+            console.log('Foto terminate!')
+            return
+        }
+        this._index++
+        this.wrapper.style.transform = `translateX(-${this._index * this._offset}%)`
+        
+        console.log('Vai a destra')
+    }
+    
+    
     goPrev(){
-
+        if(this._index == 0 ){
+            console.log('Non puoi tornare indietro')
+            return
+        }
+        this._index--
+        this.wrapper.style.transform = `translateX(-${this._index * this._offset}%)`
+        console.log('Vai a sinistra')
     }
 }
