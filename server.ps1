226b616f5ea8459ba4636e2a06fa38640e443f6a
Edoardo <e.cavallo13@studenti.uniba.it>
2020-07-30 12:23:28 +0200

    Settato il server windows su porta 80

--- server.ps1
+++ server.ps1
@@ -1,4 +1,4 @@
 $ipaddress = Get-NetIPAddress | ForEach-Object{ if($_.PrefixOrigin -eq 'Dhcp'){ return $_} }
 
 Start "http://$($ipaddress.IPAddress)"
-python -m http.server 8080 --bind $ipaddress.IPAddress
+python -m http.server 80 --bind $ipaddress.IPAddress
